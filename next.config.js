01a8ef4476288377033b50030b86f8f082815986
dandiagusatwork <DandiAgusAtWork@gmail.com>
2024-11-14 12:23:39 +0700

    add: register Api

--- next.config.js
+++ next.config.js
@@ -1,4 +1,11 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+    async rewrites() {
+        return [{
+            source: "/api/:path*", // Define the endpoint pattern in your frontend
+            destination: "https://techtest.youapp.ai/api/:path*", // Proxy to your backend server
+        }, ];
+    },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
